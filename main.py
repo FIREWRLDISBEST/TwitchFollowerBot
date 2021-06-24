377cd7c2e2862414a9a7ef06257bd4e997ad1371
Huguitis <65133800+Huguitis@users.noreply.github.com>
2021-06-24 16:04:41 +0200

    v2

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import keep_alive
+from difflib import SequenceMatcher
 from discord.ext import commands
+from lxml import html
 import subprocess
 import threading
-import aiofiles
 import discord
 import asyncio
 import aiohttp
@@ -10,20 +10,20 @@ import random
 import ctypes
 import re
 import os
-keep_alive.keep_alive()
-
-token = 'YOUR DISCORD TOKEN'
+from keep import keep
+ 
+token = 'add-a-token-here'
 prefix = '/'
-
+ 
 intents = discord.Intents().all()
 bot = commands.Bot(command_prefix=prefix, case_insensitive=True, intents=intents)
 bot.remove_command('help')
-
-administrators = ['OWNERID','OWNERID2']
-chat_channel = 826929216989167677
-bots_channel = 835276979574013952
+ 
+administrators = [499218322261540865] 
+chat_channel = 835851484281176077
+bots_channel = 835851858903695370
 queue = []
-
+ 
 def zoom():
     while True:
         try:
@@ -31,9 +31,9 @@ def zoom():
             subprocess.run([f'{task}', f'{arg1}', f'{arg2}'])
         except:
             pass
-
+ 
 threading.Thread(target=zoom).start()
-
+ 
 @bot.event
 async def on_ready():
     print(f'Servers: {len(bot.guilds)}')
@@ -43,15 +43,15 @@ async def on_ready():
     # bot.loop.create_task(status())
     while True:
         members = sum([guild.member_count for guild in bot.guilds])
-        activity = discord.Activity(type=discord.ActivityType.watching, name=f'{members} users!')
+        activity = discord.Activity(type=discord.ActivityType.watching, name=f'{members} users | ! twent v3')
         await bot.change_presence(activity=activity)
         await asyncio.sleep(60)
-
+ 
 @bot.event
 async def on_member_join(member):
     channel = await bot.fetch_channel(bots_channel)
-    await channel.send(f'Welcome to **zoom**, {member.mention}.\nType `/help` to get started! Support: https://discord.gg/ZxeavDHumA ')
-
+    await channel.send(f'Welcome to **Twent**, {member.mention}.\nType `/help` to get started! Yey!')
+ 
 @bot.event
 async def on_command_error(ctx, error: Exception):
     if ctx.channel.id == bots_channel:
@@ -72,22 +72,19 @@ async def on_command_error(ctx, error: Exception):
             await ctx.message.delete()
         except:
             pass
-
+ 
 @bot.command()
 async def help(ctx):
     print(f'{ctx.author} | {ctx.author.id} -> /help')
     if ctx.channel.type != discord.ChannelType.private:
-        embed = discord.Embed(color=16379747)
-        embed.add_field(name='Help, https://discord.gg/ZxeavDHumA', value='`/help`', inline=True)
-        embed.add_field(name='Open Ticket', value='`/ticket`', inline=True)
-        embed.add_field(name='Close Ticket', value='`/close`', inline=True)
-        embed.add_field(name='Tasks', value='`/tasks`', inline=True)
-        embed.add_field(name='Twitch Followers', value='`/tfollow (channel)`', inline=True)
-        embed.add_field(name='⭐ Twitch Spam', value='`/tspam (channel) (message)`', inline=True)
-        embed.add_field(name='Roblox Followers', value='`/rfollow (user id)`', inline=True)
-        embed.add_field(name='Roblox Templates', value='`/rget (asset id)`', inline=True)
+        embed = discord.Embed(color=16777215)
+        embed.add_field(name='🎨 Help', value='`/help`', inline=True)
+        embed.add_field(name='🎃 Tasks', value='`/tasks`', inline=True)
+        embed.add_field(name='🎉 Twitch Followers', value='`/tfollow (channel)`', inline=True)
+        embed.add_field(name='✨ Twitch Spam', value='`/tspam (channel) (message)`', inline=True)
+        embed.add_field(name='🔮 Roblox Followers', value='`/rfollow (user id)`', inline=True)
         await ctx.send(embed=embed)
-
+ 
 @bot.command()
 async def ticket(ctx):
     print(f'{ctx.author} | {ctx.author.id} -> /ticket')
@@ -103,7 +100,7 @@ async def ticket(ctx):
             embed = discord.Embed(color=16379747, description='Please enter the reason for this ticket, type `/close` if you want to close this ticket.')
             await ticket_channel.send(f'{ctx.author.mention}', embed=embed)
             await ctx.message.delete()
-
+ 
 @bot.command()
 async def close(ctx):
     print(f'{ctx.author} | {ctx.author.id} -> /close')
@@ -113,23 +110,23 @@ async def close(ctx):
         elif ctx.author.id in administrators and 'ticket' in ctx.channel.name:
             await ctx.channel.delete()
         else:
-            embed = discord.Embed(color=16379747, description=f'You do not have permission to run this command!')
+            embed = discord.Embed(color=16777215, description=f'You do not have permission to run this command!')
             await ctx.send(embed=embed)
-
+ 
 @bot.command()
 async def tasks(ctx):
     print(f'{ctx.author} | {ctx.author.id} -> /tasks')
     if ctx.channel.type != discord.ChannelType.private:
         if ctx.channel.id == bots_channel:
-            embed = discord.Embed(color=16379747, description=f'`{len(queue)}` tasks in the queue!')
+            embed = discord.Embed(color=16777215, description=f'`{len(queue)}` tasks in the queue!')
             await ctx.send(embed=embed)
         else:
             await ctx.message.delete()
-
+ 
 tfollow_cooldown = []
-
+ 
 @bot.command()
-@commands.cooldown(1, 200, type=commands.BucketType.user)
+@commands.cooldown(1, 100, type=commands.BucketType.user)
 async def tfollow(ctx, channel, amount: int=None):
     print(f'{ctx.author} | {ctx.author.id} -> /tfollow {channel}')
     if ctx.channel.type != discord.ChannelType.private:
@@ -162,12 +159,12 @@ async def tfollow(ctx, channel, amount: int=None):
                     bronze = discord.utils.get(ctx.guild.roles, name='Bronze')
                     if bronze in ctx.author.roles:
                         max_amount += 100
-                    booster = discord.utils.get(ctx.guild.roles, name='Booster')
+                    booster = discord.utils.get(ctx.guild.roles, name='#boost')
                     if booster in ctx.author.roles:
-                        max_amount += 75
-                    _75 = discord.utils.get(ctx.guild.roles, name='+75')
+                        max_amount += 5000
+                    _75 = discord.utils.get(ctx.guild.roles, name='Premium+')
                     if _75 in ctx.author.roles:
-                        max_amount += 75
+                        max_amount += 1000
                     _25 = discord.utils.get(ctx.guild.roles, name='+25')
                     if _25 in ctx.author.roles:
                         max_amount += 25
@@ -177,7 +174,7 @@ async def tfollow(ctx, channel, amount: int=None):
                     _5 = discord.utils.get(ctx.guild.roles, name='+5')
                     if _5 in ctx.author.roles:
                         max_amount += 5
-                    max_amount += 25
+                    max_amount += 50
                     if amount is None:
                         amount = max_amount
                     elif amount > max_amount:
@@ -187,27 +184,27 @@ async def tfollow(ctx, channel, amount: int=None):
                         if premium in ctx.author.roles:
                             position = len(queue) + 1
                             # embed = discord.Embed(color=16379747, description=f'Added `tfollow-{channel}-{amount}` to queue! (`1/{position}`)')
-                            embed = discord.Embed(color=16379747, description=f'Adding `{amount}` followers to `{channel}`! (`1/{position}`)')
+                            embed = discord.Embed(color=16777215, description=f'Adding `{amount}` followers to `{channel}`! (`1/{position}`)') 
                             await ctx.send(embed=embed)
                             queue.insert(0, f'tfollow-{channel}-{amount}')
                         else:
                             position = len(queue) + 1
                             # embed = discord.Embed(color=16379747, description=f'Added `tfollow-{channel}-{amount}` to queue! (`{position}/{position}`)')
-                            embed = discord.Embed(color=16379747, description=f'Adding `{amount}` followers to `{channel}`! (`{position}/{position}`)')
+                            embed = discord.Embed(color=16379747, description=f'Adding `{amount}` followers to `{channel}`! (`1/{position}`)') 
                             await ctx.send(embed=embed)
                             queue.append(f'tfollow-{channel}-{amount}')
                         if ctx.author.id not in administrators:
                             tfollow_cooldown.append(str(channel.lower()))
-                            await asyncio.sleep(600)
+                            await asyncio.sleep(300)
                             tfollow_cooldown.remove(str(channel.lower()))
                 except:
-                    embed = discord.Embed(color=16379747, description='An error has occured while attempting to run this command! Support: https://discord.gg/ZxeavDHumA')
+                    embed = discord.Embed(color=16379747, description=f'Error, try again `{channel}`!')
                     await ctx.send(embed=embed)
                     tfollow.reset_cooldown(ctx)
         else:
             await ctx.message.delete()
             tfollow.reset_cooldown(ctx)
-
+ 
 @bot.command()
 @commands.cooldown(1, 600, type=commands.BucketType.user)
 async def tspam(ctx, channel, *, msg):
@@ -226,11 +223,11 @@ async def tspam(ctx, channel, *, msg):
                         amount = max_amount
                     if amount <= max_amount:
                         position = len(queue) + 1
-                        embed = discord.Embed(color=16379747, description=f'Added `tspam-{channel}-{msg}` to queue! (`1/{position}`)')
+                        embed = discord.Embed(color=16379747, description=f'Added `tspam-{channel}-{msg}` to queue!')
                         await ctx.send(embed=embed)
                         queue.insert(0, f'tspam-{channel}-{msg}')
                 except:
-                    embed = discord.Embed(color=16379747, description='An error has occured while attempting to run this command! Support: https://discord.gg/ZxeavDHumA')
+                    embed = discord.Embed(color=16379747, description=f'Added `tspam {channel} {msg}` to queue! ')
                     await ctx.send(embed=embed)
                     tspam.reset_cooldown(ctx)
             else:
@@ -239,9 +236,9 @@ async def tspam(ctx, channel, *, msg):
         else:
             embed = discord.Embed(color=16379747, description='You do not have permission to run this command!')
             await ctx.send(embed=embed)
-
+ 
 rfollow_cooldown = []
-
+ 
 @bot.command()
 @commands.cooldown(1, 600, type=commands.BucketType.user)
 async def rfollow(ctx, user_id, amount: int=None):
@@ -269,12 +266,12 @@ async def rfollow(ctx, user_id, amount: int=None):
                         premium = discord.utils.get(ctx.guild.roles, name='Premium')
                         if premium in ctx.author.roles:
                             position = len(queue) + 1
-                            embed = discord.Embed(color=16379747, description=f'Adding `{amount}` followers to `{user_id}`! (`1/{position}`)')
+                            embed = discord.Embed(color=16777215, description=f'Adding `{amount}` followers to `{user_id}`! (`1/{position}`)') 
                             await ctx.send(embed=embed)
                             queue.insert(0, f'rfollow-{user_id}-{amount}')
                         else:
                             position = len(queue) + 1
-                            embed = discord.Embed(color=16379747, description=f'Adding `{amount}` followers to `{user_id}`! (`{position}/{position}`)')
+                            embed = discord.Embed(color=16777215, description=f'Adding `{amount}` followers to `{user_id}`! (`{position}/{position}`)') 
                             await ctx.send(embed=embed)
                             queue.append(f'rfollow-{user_id}-{amount}')
                         if ctx.author.id not in administrators:
@@ -282,41 +279,58 @@ async def rfollow(ctx, user_id, amount: int=None):
                             await asyncio.sleep(600)
                             rfollow_cooldown.remove(str(user_id))
                 except:
-                    embed = discord.Embed(color=16379747, description='An error has occured while attempting to run this command! Support: https://discord.gg/ZxeavDHumA')
+                    embed = discord.Embed(color=16379747, description='An error has occured while attempting to run this command!')
                     await ctx.send(embed=embed)
                     rfollow.reset_cooldown(ctx)
         else:
             await ctx.message.delete()
             rfollow.reset_cooldown(ctx)
-
+ 
 @bot.command()
-async def rget(ctx, asset_id):
-    print(f'{ctx.author} | {ctx.author.id} -> /rget {asset_id}')
+async def trivia(ctx):
+    print(f'{ctx.author} | {ctx.author.id} -> /trivia')
     if ctx.channel.type != discord.ChannelType.private:
-        if ctx.channel.id == bots_channel:
-            try:
-                async with aiohttp.ClientSession() as session:
-                    async with session.get(f'https://assetdelivery.roblox.com/v1/asset?id={asset_id}') as r:
-                        r = await r.text()
-                    async with session.get(f'https://assetdelivery.roblox.com/v1/asset?id=' + re.search('id=(.*)</url>', r).group(1)) as r:
-                        r = await r.read()
-                try:
-                    f = await aiofiles.open(f'{asset_id}.png', mode='wb')
-                    await f.write(r)
-                    await f.close()
-                    embed = discord.Embed(color=16379747)
-                    file = discord.File(f'{asset_id}.png')
-                    embed.set_image(url=f'attachment://{asset_id}.png')
-                    await ctx.send(embed=embed, file=file)
-                finally:
+        if ctx.author.id in administrators:
+            await ctx.message.delete()
+            async with aiohttp.ClientSession() as session:
+                while True:
                     try:
-                        os.remove(f'{asset_id}.png')
+                        # question, answer = random.choice(list(questions.items()))
+                        while True:
+                            async with session.get('https://opentdb.com/api.php?amount=1&type=multiple') as r:
+                                r = await r.json()
+                                question = html.fromstring(str(r['results'][0]['question'])).text_content()
+                                answer = r['results'][0]['correct_answer']
+                                if 'which' in question.lower():
+                                    pass
+                                else:
+                                    break
+                        embed = discord.Embed(color=16379747, description=f'**{question}**\n\nReward: **1080 twitch followers**')
+                        await ctx.send(embed=embed)
+                        def check(message: discord.Message):
+                            return str(message.content).lower() == str(answer).lower()
+                            return SequenceMatcher(None, str(answer).lower(), str(message.content).lower()).ratio() > float(0.5) and message.channel.id == chat_channel
+                        _answer = await bot.wait_for('message', check=check, timeout=20)
+                        try:
+                            embed = discord.Embed(color=16379747, description=f'{_answer.author.mention} has answered the question correctly!\n\nAnswer: **{answer}**')
+                            await ctx.send(embed=embed)
+                            embed = discord.Embed(color=16379747, description=f'{_answer.author.mention} send your twitch channel to claim the reward!')
+                            await ctx.send(embed=embed)
+                            def _check(message: discord.Message):
+                                return message.author.id == _answer.author.id and message.channel.id == chat_channel
+                            _channel = await bot.wait_for('message', check=_check, timeout=20)
+                            queue.append(f'tfollow-{_channel.content}-50')
+                        except asyncio.TimeoutError:
+                            pass
+                    except asyncio.TimeoutError:
+                        embed = discord.Embed(color=16379747, description=f'Nobody answered the question correctly.\n\nCorrect Answer: **{answer}**')
+                        await ctx.send(embed=embed)
                     except:
                         pass
-            except:
-                embed = discord.Embed(color=16379747, description='An error has occured while attempting to run this command! Support: https://discord.gg/ZxeavDHumA')
-                await ctx.send(embed=embed)
+                    await asyncio.sleep(5)
         else:
             await ctx.message.delete()
-
+ 
+ 
+keep()
 bot.run(token)
